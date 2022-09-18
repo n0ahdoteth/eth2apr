b67c44e49a4824ac2861b9ceaac6963f10273c7d
noahdoteth <113805751+noahdoteth@users.noreply.github.com>
2022-09-18 17:32:08 -0400

    Add files via upload

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -5,9 +5,10 @@ const AppInfo = () => {
         <div>
             <div className="app-info">
                 <ul>
-                    <li>Annual percentage rate (APR) is in ETH terms, not USD</li>
-                    <li>Calculation based off of <a href="https://docs.google.com/spreadsheets/d/15tmPOvOgi3wKxJw7KQJKoUe-uonbYR6HF7u83LR5Mj4/edit#gid=1018097491">@StakeETH's work</a></li>
-                    <li>Data from <a href="https://coingecko.com">Coingecko</a></li>
+                    <li>Data updated/APR recalculated every few minutes</li>
+                    <li>Annual percentage rate (APR) is in Ether terms, not USD</li>
+                    <li>Calculation based off of <a href="https://docs.google.com/spreadsheets/d/15tmPOvOgi3wKxJw7KQJKoUe-uonbYR6HF7u83LR5Mj4/edit#gid=1018097491" target="_blank">@StakeETH's work</a></li>
+                    <li>Ethereum data from <a href="https://coingecko.com" target="_blank">Coingecko API</a></li>
                 </ul>
             </div>
         </div>
